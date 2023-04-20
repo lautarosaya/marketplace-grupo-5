5987a81efd20aef18423d650efc6a1f2bc939b2a
lautarosaya <lautarosaya0012@gmail.com>
2023-04-20 02:35:53 -0300

    product vista

---
+++ src/routes/product-router.js
@@ -0,0 +1,7 @@
+const { Router } = require("express");
+const router = Router();
+const productRouter = require("../controllers/product-controller");
+
+router.get("/showProduct", productRouter.show);
+
+module.exports = router;
